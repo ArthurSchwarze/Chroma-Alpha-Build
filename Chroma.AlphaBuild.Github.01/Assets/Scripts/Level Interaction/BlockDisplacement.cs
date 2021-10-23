05d463dfa5ecd4db55b7c07608e73098af08013d
ArthurSchwarze <arthur.schwarze@students.ksba.ch>
2021-10-23 10:47:24 +0200

    LevelDesign1

---
+++ Chroma.AlphaBuild.Github.01/Assets/Scripts/Level Interaction/BlockDisplacement.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockDisplacement : MonoBehaviour
+{
+    public GameObject[] pathNodes;
+    public int currentNode = 0;
+    public int[] waitNode;
+
+    public bool mooving = false;
+    public bool moovesCircle = false;
+    public bool wayBack = false;
+
+    private float difference;
+
+    public int speed = 1;
+    public int waitTime = 1;
+
+    private Vector3 movementVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*if (pathNodes != null)
+        {
+            transform.position = pathNodes[0].transform.position;
+            
+            if (pathNodes.Length != 1)
+            {
+                movementVector = CreateSizedVector(pathNodes[1].transform.position - transform.position);
+                currentNode = 1;
+            }
+        }
+        else
+        {
+            mooving = false;
+        }*/
+
+        StartCoroutine(movementWait());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mooving)
+        {
+            if (movementVector != Vector3.zero)
+            {
+                CheckNode(); 
+            }
+            transform.position += movementVector * Time.deltaTime;
+        }
+        else
+        {
+
+        }
+    }
+
+
+    private void CheckNode()
+    {
+        difference = (pathNodes[currentNode].transform.position - transform.position).sqrMagnitude - (movementVector * Time.deltaTime).sqrMagnitude;
+
+        if (difference < 0)
+        {
+            StartCoroutine(movementWait());
+        }
+    }
+
+    private Vector3 CreateSizedVector(Vector3 vector)
+    {
+        var Coordinates = new List<float> { Mathf.Abs(vector.x), Mathf.Abs(vector.y), Mathf.Abs(vector.z) };
+        float maxValue = Mathf.Max(Coordinates.ToArray());
+
+        return (vector * speed / Mathf.Abs(maxValue));
+    }
+
+    IEnumerator movementWait()
+    {
+        transform.position = pathNodes[currentNode].transform.position;
+        movementVector = Vector3.zero;
+        
+        foreach (int element in waitNode)
+        {
+            if (element == currentNode)
+            {
+                yield return new WaitForSecondsRealtime(waitTime);
+            }
+        }
+
+        if (currentNode == 0)
+        {
+            wayBack = false;
+        }  
+        else if (currentNode + 1 == pathNodes.Length)
+        {
+            if (moovesCircle)
+            {
+                wayBack = true;
+            }
+            else
+            {
+                currentNode = -1;
+            }
+            
+        }
+        
+        if (wayBack)
+        {
+            currentNode--;
+        }   
+        else
+        {
+            currentNode++;
+        }
+        movementVector = CreateSizedVector(pathNodes[currentNode].transform.position - transform.position);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("collision.gameObject.layer");
+    }
+}
---
+++ Chroma.AlphaBuild.Github.01/Assets/Scripts/Level Interaction/TriggerDoorController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoorController : MonoBehaviour
+{
+    [SerializeField] private Animator myDoor = null;
+
+    [SerializeField] private int openNumber = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.GetComponent<GravityGameObject>())
+        {
+            openNumber++;
+            MoveDoor();
+        }
+
+        /*
+        else if (collision.gameObject.GetComponent<PlayerMovement>())
+        {
+            MoveDoor();
+            Debug.Log("Player");
+        }
+        */
+
+        if (LayerMask.NameToLayer("Ground") == collision.gameObject.layer)
+        {
+            return;
+        }
+
+        openNumber++;
+        MoveDoor();
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<GravityGameObject>())
+        {
+            openNumber--;
+            MoveDoor();
+        }
+
+        if (LayerMask.NameToLayer("Ground") == collision.gameObject.layer)
+        {
+            return;
+        }
+
+        openNumber--;
+        MoveDoor();
+
+
+    }
+
+    private void MoveDoor()
+    {
+        float time = myDoor.GetCurrentAnimatorStateInfo(0).normalizedTime;
+
+        if (openNumber == 0)
+        {
+            if (time < 1.0f)
+            {
+                myDoor.Play("DoorClose", 0, 1.0f - time);
+                return;
+            }
+
+            myDoor.Play("DoorClose", 0, 0.0f);
+        }
+
+        else if (openNumber == 1)
+        {
+            if (time < 1.0f)
+            {
+                myDoor.Play("DoorOpen", 0, 1.0f - time);
+                return;
+            }
+            myDoor.Play("DoorOpen", 0, 0.0f);
+        }
+
+    }
+}
