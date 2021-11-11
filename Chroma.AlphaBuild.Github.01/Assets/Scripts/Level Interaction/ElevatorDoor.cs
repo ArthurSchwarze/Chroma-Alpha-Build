db1cca3733df84224f1285bc602700a78db86996
Voltagami <mavin.wrauch@gmail.com>
2021-11-11 10:34:21 +0100

    opt elevator (not finished)

--- Chroma.AlphaBuild.Github.01/Assets/Scripts/Level Interaction/ElevatorDoor.cs
+++ Chroma.AlphaBuild.Github.01/Assets/Scripts/Level Interaction/ElevatorDoor.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class ElevatorDoor : MonoBehaviour
 {
-    public float speed;
-    public Vector3 targetPos;
+    public float speed, speed2;
+    public Vector3 targetPos, targetPos2;
+    private bool finished1, start = true, now;
 
     // Update is called once per frame
     void Update()
     {
-        transform.localPosition = Vector3.MoveTowards(this.transform.localPosition, targetPos, speed * Time.deltaTime);
+        if (start)
+        {
+            transform.localPosition = Vector3.MoveTowards(this.transform.localPosition, targetPos, speed * Time.deltaTime);
+        }
+
+        if (this.transform.localPosition == targetPos || now)
+        {
+            finished1 = true;
+            now = true;
+        }
+
+        if (finished1)
+        {
+            start = false;
+            Invoke("SecondDoorMove", 3f);
+        }
+    }
+
+    void SecondDoorMove()
+    {
+        transform.localPosition = Vector3.MoveTowards(targetPos, targetPos2, speed2 * Time.deltaTime);
     }
 }
