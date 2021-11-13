f1de77594ae214f110e5d539000ca74cf26b13bc
ArthurSchwarze <arthur.schwarze@students.ksba.ch>
2021-11-13 16:40:34 +0100

    Text Change 1

--- Chroma.AlphaBuild.Github.01/Assets/Scripts/Level Interaction/ElevatorDoor.cs
+++ Chroma.AlphaBuild.Github.01/Assets/Scripts/Level Interaction/ElevatorDoor.cs
@@ -40,7 +40,7 @@ public class ElevatorDoor : MonoBehaviour
                 openSound.PlayDelayed(2.5f);
             }
             start = false;
-            Invoke("SecondDoorMove", 3f);
+            Invoke("SecondDoorMove", 2.5f);
         }
     }
 
